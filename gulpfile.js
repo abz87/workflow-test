c50299916ad3c6c40cefdbddd782b85206d08e1e
abdulzoo <abdul.rehman@zoocomms.com>
2017-05-30 17:18:11 +0100

    Updated style.scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var cleanCSS = require('gulp-clean-css');
 var concatCSS = require('gulp-concat-css');
 var browserSync = require('browser-sync').create();
 var sass = require ('gulp-sass');
-var jshint = require ('gulp-jshint');
 
 gulp.task('sass', function() {
 	gulp.src ('src/sass/style.scss')
@@ -13,14 +12,6 @@ gulp.task('sass', function() {
 		.pipe(cleanCSS());
 });
 
-gulp.task('js', function() {
-	gulp.src ('src/javascript/app.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(jshint.reporter('fail'))
-		.pipe(gulp.dest('dist/resource/javascript'))
-		.pipe(browserSync.stream());
-});
 
 gulp.task('serve', function () {
 	browserSync.init({
@@ -30,10 +21,5 @@ gulp.task('serve', function () {
 
 gulp.task('default', ['serve','sass'], function () {
 	gulp.watch('src/sass/*.scss', ['sass']);
-	gulp.watch('./*.html').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['serve','js'], function () {
-	gulp.watch('src/javascript/*.jss', ['js']);
 	gulp.watch('./*.html').on('change', browserSync.reload);
 });
